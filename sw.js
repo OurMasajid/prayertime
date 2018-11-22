db1301b262cdff682e2cf97465c9b131919cd45e
akaban01 <akaban01@gmail.com>
2018-11-21 20:08:49 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -30,7 +30,10 @@ self.addEventListener('activate', function (e) {
 
 self.addEventListener('fetch', function (e) {
     //console.log('[Service Worker] Fetch', e.request.url);
-    e.respondWith(function () {
-        return fetch(e.request);
-    })
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+
 });
